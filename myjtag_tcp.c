8389578e3c87ce1f7df4282e3876836c1037cd7d
Fakhareddine SABBARI <fakhareddine.sabbari@tiempo-secure.com>
2021-10-06 10:50:25 +0200

    added myjtag

---
+++ myjtag_tcp.c
@@ -0,0 +1,136 @@
+/*
+A trial to implement a master SPI that can be used in simulation
+
+TODO : clean the code and make it areal code , be aware that a lot of function
+are wrapped around a no argument function to make calling them from verilog
+easier
+
+
+*/
+
+//#include "svdpi.h"
+#include <arpa/inet.h> // sockaddr_in, AF_INET, SOCK_STREAM, INADDR_ANY, socket etc...
+#include <fcntl.h>
+#include <stdio.h>  // perror, printf
+#include <stdlib.h> // exit, atoi
+#include <string.h> // memset
+#include <unistd.h> // read, write, close
+#define OPEN_PORT 0
+#define RECEIVE 1
+#define SETUP_JTAG 2
+#define SEND_JTAG 3
+#define SEND_TOCLIENT 4
+#define DEBUG 0
+#define BUFFER_SIZE 32
+
+
+int size;
+
+
+
+int receive(int clientFd, char *buffer, char* miso_buffer);
+int send_to_client(int clientFd, char *miso_buffer, int size);
+
+int create_socket_and_bind(int port,int* clientFd, int* serverFd, struct sockaddr_in* server, struct sockaddr_in* client );
+
+extern int jtag_server(int *tck, int *tms, int *tdi,
+                     const unsigned int tdo, int port) {
+
+  static unsigned char buffer[BUFFER_SIZE];
+  static  char miso_buffer[BUFFER_SIZE];	
+  static int size, counter, state, clock_state = 0;
+  static int clientFd, serverFd; // client and servet file descriptor
+  static struct sockaddr_in server, client;
+  static unsigned int buffer_index = 0;
+
+  switch (state) {
+  case(OPEN_PORT):
+    state= create_socket_and_bind(port,&clientFd,&serverFd, &server, &client)>0 ? RECEIVE :OPEN_PORT; 
+    return 1;
+  case (RECEIVE):
+    counter = 0;
+    buffer_index = 0;
+    size = receive(clientFd, buffer,miso_buffer);
+    state = size > 0 ? SEND_JTAG : RECEIVE;
+    return 1;
+
+  case (SEND_JTAG):
+        ;
+        int command =buffer[0];
+        *tms = (command & 1) != 0;
+        *tdi = (command & 2) != 0;
+        *tck = (command & 8) != 0;
+        if(command&4)state=SEND_TOCLIENT;
+        return 1;
+
+  case (SEND_TOCLIENT):
+    miso_buffer[0]=tdo&1;
+    state = RECEIVE;
+    int sent = send_to_client(clientFd, miso_buffer, 1);
+    if (sent < 1) {
+      perror("Couldn't send all tdo buffer to client ");
+      return -1;
+    }
+    return 1;
+  }
+}
+
+int create_socket_and_bind(int port,int* clientFd, int* serverFd, struct sockaddr_in* server, struct sockaddr_in* client ) {
+	memset(server, 0, sizeof(*server));
+
+  *serverFd = socket(AF_INET, SOCK_STREAM, 0);
+  if (*serverFd < 0) {
+    perror("Cannot create socket");
+   // exit(1);
+  }
+
+  server->sin_family = AF_INET;
+  server->sin_addr.s_addr = inet_addr("127.0.0.1"); // INADDR_ANY;
+  server->sin_port = htons(port);
+  int len = sizeof(*server);
+  if (bind(*serverFd,(struct sockaddr *)server, len) < 0) {
+    perror("Cannot bind socket");
+    exit(2);
+  }
+  if (listen(*serverFd, 10) < 0) {
+    perror("Listen error");
+    exit(3);
+  }
+
+  len = sizeof(*client);
+  printf("waiting for clients at port %d \n", port);
+  if ((*clientFd = accept(*serverFd, (struct sockaddr *)client, &len)) < 0) {
+    perror("accept error");
+    exit(4);
+  }
+  char *client_ip = inet_ntoa(client->sin_addr);
+  printf("Accepted new connection from a client %s:%d\n", client_ip,
+         ntohs(client->sin_port));
+
+  int flags = fcntl(*clientFd, F_GETFL, 0);
+  fcntl(*clientFd, F_SETFL, flags | O_NONBLOCK);
+
+  return 1;
+}
+
+int receive(int clientFd, char *buffer, char *miso_buffer) {
+
+  memset(buffer, 0, sizeof(buffer));
+  memset(miso_buffer, 0, sizeof(miso_buffer));
+  size = read(clientFd, buffer, sizeof(buffer));
+  if (size < 0) {
+    perror("read error");
+    return -1;
+  }
+  // printf("received %s from client\n", buffer);
+  return size;
+}
+
+int send_to_client(int clientFd, char *miso_buffer, int size) {
+  int sent = write(clientFd, miso_buffer, size);
+  if (sent < 0) {
+    perror("write error");
+    return -1;
+  }
+  return sent;
+}
---
+++ myjtag_tcp.sv
@@ -0,0 +1,31 @@
+module jtag_dpi(
+    output reg tck,
+    output reg tms,
+    output reg tdi,
+    input tdo,
+    input sys_clk
+);
+
+parameter port = 1234;
+
+
+
+
+
+import "DPI-C" context function int jtag_server( output bit tck,
+                                             output bit tms,
+                                             output bit tdi,
+                                             input  bit tdo,
+                                             input  int port
+); // spi() defined in file server_spi.c
+
+
+
+
+always @(posedge sys_clk)
+begin
+
+	jtag_server(spi_cs_o,spi_clk_o,spi_mosi_o,spi_miso_i,port);
+    	
+end
+endmodule
